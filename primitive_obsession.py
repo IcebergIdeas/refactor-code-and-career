407e187c86df12f35a4d6f0222a816ac4f94f475
Bryan Beecham <billygarnet@gmail.com>
2020-11-05 19:56:11 -0500

    Create primitive_obsession.py

---
+++ primitive_obsession.py
@@ -0,0 +1,7 @@
+def primitive_obsession_example(self):
+    message_type = "Important"
+    message_important_flag = 0
+    message = input("Enter a message")
+
+    # using primitive objects above
+    # creating a message object would be better
